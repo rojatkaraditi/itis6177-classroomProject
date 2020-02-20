81e0e089361b94ea2f0e287310f4f3917aa7100d
Aditi Rojatkar <rojatkaraditi@Aditis-MacBook-Pro.local>
2020-02-19 20:07:37 -0500

    aws code changes

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
+const bodyParser = require("body-parser");
 
 const app = express()
+
 const port = 3000
 
 app.use(cors());
@@ -10,7 +12,7 @@ app.use(cors());
 //app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/',(request,response)=>{
     axios.get('https://kbfafxfdz2.execute-api.us-east-1.amazonaws.com/one').then(resp=>{
-    console.log(resp);
+    response.send(resp.data.body);
     });
 });
 
