e66ccd7d8a72a7bbc4bf99cc2455000b8a26d202
Aditi Rojatkar <rojatkaraditi@Aditis-MacBook-Pro.local>
2020-02-19 19:41:55 -0500

    Code to connect to aws

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
-const express = require('express')
-const cors = require('cors')
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
+
 const app = express()
 const port = 3000
 
 app.use(cors());
 
-app.get('/', (req, res) => res.send('Hello World!'))
+//app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/',(request,response)=>{
+    axios.get('https://t3tb5kidqg.execute-api.us-east-2.amazonaws.com/default/HelloWorld').then(resp=>{
+    console.log(resp.body);
+    });
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
