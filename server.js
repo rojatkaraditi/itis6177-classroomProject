4126f0362b110547cbcf0e4f47d0dda3faa96ab5
Aditi Rojatkar <rojatkaraditi@Aditis-MacBook-Pro.local>
2020-02-19 19:47:56 -0500

    Reverting changes

--- server.js
+++ server.js
@@ -7,11 +7,11 @@ const port = 3000
 
 app.use(cors());
 
-//app.get('/', (req, res) => res.send('Hello World!'))
-app.get('/',(request,response)=>{
-    axios.get('https://t3tb5kidqg.execute-api.us-east-2.amazonaws.com/default/HelloWorld').then(resp=>{
-    console.log(resp.body);
-    });
-});
+app.get('/', (req, res) => res.send('Hello World!'))
+// app.get('/',(request,response)=>{
+//     axios.get('https://t3tb5kidqg.execute-api.us-east-2.amazonaws.com/default/HelloWorld').then(resp=>{
+//     console.log(resp.body);
+//     });
+// });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
